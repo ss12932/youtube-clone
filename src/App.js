6aa02e890b6c71f7fca7b7eac67aa6621ead4acd
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-22 13:07:03 +0100

    routing completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 import React from 'react';
 
-const App = () => {
-  return <div></div>;
-};
+const App = () => (
+  <Router>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </Router>
+);
 
 export default App;
