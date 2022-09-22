800e1721e6171535fe3320026524cbe9c28f7237
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-22 17:36:18 +0100

    finish initial Feed layout + sidebar comp made

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1, color: '#ffffff' }}
+        >
+          Copyright 2022 youtube-clone
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
