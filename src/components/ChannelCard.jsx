cbf00585e77300aa3967ddd35dd7b873488d88d4
ss12932 <ss12932@my.bristol.ac.uk>
2022-10-16 23:19:01 +0100

    fix responsiveness for channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,18 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 const ChannelCard = ({ channelDetail }) => (
-  <Box sx={{ boxShadow: 'none', borderRadius: '20px' }}>
+  <Box
+    sx={{
+      boxShadow: 'none',
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: ' center',
+      alignItems: 'center',
+      width: { xs: '356px', md: '320px' },
+      height: '326px',
+      margin: 'auto',
+    }}
+  >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent
         sx={{
@@ -33,6 +44,14 @@ const ChannelCard = ({ channelDetail }) => (
           {channelDetail?.snippet?.title}
           <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
         </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography>
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount.toLocaleString(),
+            )}{' '}
+            Subscribers
+          </Typography>
+        )}
       </CardContent>
     </Link>
   </Box>
