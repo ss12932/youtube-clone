ec9ce5eb507b82c5d3f48083c61a2abc9b658dd5
ss12932 <ss12932@my.bristol.ac.uk>
2022-10-16 20:35:24 +0100

    create videocard component styling

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  cardActionAreaClasses,
+} from '@mui/material';
+import { checkCircle } from '@mui/icons-material';
+
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from '../utils/constants';
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+        <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+          <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography>
+              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            </Typography>
+          </Link>
+        </CardContent>
+      </Link>
+    </Card>
+  );
+};
+
+export default VideoCard;
