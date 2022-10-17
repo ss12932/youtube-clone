ec444cc64f727dc92aca3e28db0ce3c41be4d45e
ss12932 <ss12932@my.bristol.ac.uk>
2022-10-17 16:33:08 +0100

    finish channel detail component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -21,7 +21,25 @@ const ChannelDetail = () => {
       (data) => setVideos(data?.items),
     );
   }, [id]);
-  return <div>{id}</div>;
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+            zIndex: 10,
+            height: '300px',
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
